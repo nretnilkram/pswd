3592000a0e1ccea32a4f3c565b1a6cefa21f14d6
Mark Lintern <mark.lintern@greenlight.guru>
2021-10-29 14:01:36 -0400

    additional tests

--- pswd_test.go
+++ pswd_test.go
@@ -1,10 +1,11 @@
 package pswd
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestPassword(t *testing.T) {
+func TestPasswordLength(t *testing.T) {
 	cases := []struct {
 		in     int
 		want   int
@@ -27,6 +28,90 @@ func TestPassword(t *testing.T) {
 		got := len(Password(c.in, c.weight))
 		if got != c.want {
 			t.Errorf("len(Password(%q)) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestPasswordOnlySym(t *testing.T) {
+	cases := []struct {
+		in     int
+		weight PasswordWeight
+	}{
+		{24, PasswordWeight{Lower: 0, Upper: 0, Digit: 0, Symbol: 1}},
+		{3, PasswordWeight{Lower: 0, Upper: 0, Digit: 0, Symbol: 1}},
+		{200, PasswordWeight{Lower: 0, Upper: 0, Digit: 0, Symbol: 1}},
+		{2000, PasswordWeight{Lower: 0, Upper: 0, Digit: 0, Symbol: 1}},
+	}
+	for _, c := range cases {
+		got := Password(c.in, c.weight)
+		pw_array := []rune(got)
+		for i := 0; i < len(pw_array); i++ {
+			if !strings.Contains(Symbols, string(pw_array[i])) {
+				t.Errorf("Password(%q) containers something other than symbols: %q", c.in, got)
+			}
+		}
+	}
+}
+
+func TestPasswordOnlyDigit(t *testing.T) {
+	cases := []struct {
+		in     int
+		weight PasswordWeight
+	}{
+		{24, PasswordWeight{Lower: 0, Upper: 0, Digit: 1, Symbol: 0}},
+		{3, PasswordWeight{Lower: 0, Upper: 0, Digit: 1, Symbol: 0}},
+		{200, PasswordWeight{Lower: 0, Upper: 0, Digit: 1, Symbol: 0}},
+		{2000, PasswordWeight{Lower: 0, Upper: 0, Digit: 1, Symbol: 0}},
+	}
+	for _, c := range cases {
+		got := Password(c.in, c.weight)
+		pw_array := []rune(got)
+		for i := 0; i < len(pw_array); i++ {
+			if !strings.Contains(Digits, string(pw_array[i])) {
+				t.Errorf("Password(%q) containers something other than symbols: %q", c.in, got)
+			}
+		}
+	}
+}
+
+func TestPasswordOnlyLower(t *testing.T) {
+	cases := []struct {
+		in     int
+		weight PasswordWeight
+	}{
+		{24, PasswordWeight{Lower: 1, Upper: 0, Digit: 0, Symbol: 0}},
+		{3, PasswordWeight{Lower: 1, Upper: 0, Digit: 0, Symbol: 0}},
+		{200, PasswordWeight{Lower: 1, Upper: 0, Digit: 0, Symbol: 0}},
+		{2000, PasswordWeight{Lower: 1, Upper: 0, Digit: 0, Symbol: 0}},
+	}
+	for _, c := range cases {
+		got := Password(c.in, c.weight)
+		pw_array := []rune(got)
+		for i := 0; i < len(pw_array); i++ {
+			if !strings.Contains(LowerLetters, string(pw_array[i])) {
+				t.Errorf("Password(%q) containers something other than symbols: %q", c.in, got)
+			}
+		}
+	}
+}
+
+func TestPasswordOnlyUpper(t *testing.T) {
+	cases := []struct {
+		in     int
+		weight PasswordWeight
+	}{
+		{24, PasswordWeight{Lower: 0, Upper: 1, Digit: 0, Symbol: 0}},
+		{3, PasswordWeight{Lower: 0, Upper: 1, Digit: 0, Symbol: 0}},
+		{200, PasswordWeight{Lower: 0, Upper: 1, Digit: 0, Symbol: 0}},
+		{2000, PasswordWeight{Lower: 0, Upper: 1, Digit: 0, Symbol: 0}},
+	}
+	for _, c := range cases {
+		got := Password(c.in, c.weight)
+		pw_array := []rune(got)
+		for i := 0; i < len(pw_array); i++ {
+			if !strings.Contains(UpperLetters, string(pw_array[i])) {
+				t.Errorf("Password(%q) containers something other than symbols: %q", c.in, got)
+			}
 		}
 	}
 }
